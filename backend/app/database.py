134b05504976b56cc3b118675c4558fcb8eb8917
trickyholley <tricky@holley.dev>
2026-08-01 15:40:59 -0400

    adjusted connect_args

--- backend/app/database.py
+++ backend/app/database.py
@@ -12,6 +12,7 @@ from app.config import settings
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=settings.DEBUG,
+    connect_args={"statement_cache_size": 0},
 )
 async_session_factory = async_sessionmaker(
     engine,

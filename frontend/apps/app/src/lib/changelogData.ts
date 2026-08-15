b241071e34c9d5fd26bb6b0cb334a76326499369
trickyholley <25308999+trickyholley@users.noreply.github.com>
2026-08-14 21:37:19 -0400

    added patch note

--- frontend/apps/app/src/lib/changelogData.ts
+++ frontend/apps/app/src/lib/changelogData.ts
@@ -12,6 +12,11 @@ export interface ChangelogEntry {
  */
 export const CHANGELOG_ENTRIES: ChangelogEntry[] = [
   {
+    version: "0.14.0",
+    date: "2026-08-14",
+    message: "Added a reverse toggle for viewing cards in the deck tab",
+  },
+  {
     version: "0.13.0",
     date: "2026-08-12",
     message: "Added a widget for Android to show the user's spread of the day.",

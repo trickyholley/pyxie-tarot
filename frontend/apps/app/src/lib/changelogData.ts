034599c69de6b329f900b5084f8391aea6f3078b
trickyholley <25308999+trickyholley@users.noreply.github.com>
2026-08-18 16:44:05 -0400

    patch note

--- frontend/apps/app/src/lib/changelogData.ts
+++ frontend/apps/app/src/lib/changelogData.ts
@@ -12,6 +12,11 @@ export interface ChangelogEntry {
  */
 export const CHANGELOG_ENTRIES: ChangelogEntry[] = [
   {
+    version: "0.19.0",
+    date: "2026-08-18",
+    message: "Added a Contact form. Talk to us sometime!",
+  },
+  {
     version: "0.18.0",
     date: "2026-08-17",
     message: "Added a Privacy Policy, can be accessed in Settings.",
